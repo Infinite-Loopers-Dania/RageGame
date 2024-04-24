3dfd1aead7776c2b644a114b0ce9916c802432c3
BrokenModem <112692508+BrokenModem@users.noreply.github.com>
2024-04-24 12:54:06 +0200

    ye
    eye

--- Source/RageGame/MyPlayerController.cpp
+++ Source/RageGame/MyPlayerController.cpp
@@ -3,3 +3,31 @@
 
 #include "MyPlayerController.h"
 
+
+AMyPlayerController::AMyPlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	JumpThreshold = 300.0f;
+
+}
+
+void AMyPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CalculateCanJump();
+
+}
+
+void AMyPlayerController::CalculateCanJump()
+{
+
+	UPrimitiveComponent* PrimitiveComponent = pCharacter->GetMesh();
+
+	FVector LinearVelocity = PrimitiveComponent->GetPhysicsLinearVelocityAtPoint(FVector::ZeroVector);
+
+	float VelocityLengthXY = LinearVelocity.Size2D();
+
+	bCanJump = (VelocityLengthXY > JumpThreshold);
+}
--- Source/RageGame/MyPlayerController.h
+++ Source/RageGame/MyPlayerController.h
@@ -4,14 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "GameFrameWork/Character.h"
+#include "Components/PrimitiveComponent.h"
 #include "MyPlayerController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class RAGEGAME_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AMyPlayerController();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player")
+	bool bCanJump;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Player")
+	float JumpThreshold;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Player")
+	ACharacter* pCharacter;
+
+private:
+
+	void CalculateCanJump();
+
 };
+
